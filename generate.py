c50d075dac14a70e1394a823d9f284a0d6f1d4c1
Mike <mikekearney85@hotmail.com>
2020-11-24 22:14:48 +0000

    ammded generate for new model

--- generate.py
+++ generate.py
@@ -7,21 +7,21 @@ sess = gpt2.start_tf_sess()
 gpt2.load_gpt2(sess, run_name="run2")
 
 def make_text(user_input):
-    def parse_text(text):
-        list = text.split('\n')
-        list_start = list[:2]
-        string_start = " ".join(list_start)
+    # def parse_text(text):
+        # list = text.split('\n')
+        # list_start = list[:2]
+        # string_start = " ".join(list_start)
 
-        list_end = list[2:-1]
-        string_end = "\n".join(list_end)
+        # list_end = list[2:-1]
+        # string_end = "\n".join(list_end)
 
-        string_full = string_start + string_end
+        # string_full = string_start + string_end
 
-        return string_full
+        # return string_full
 
-    text = gpt2.generate(sess, length=200, return_as_list=True ,temperature=0.8, prefix= user_input, include_prefix=False, truncate='<|endoftext|>')[0]
+    text = gpt2.generate(sess, length=200, return_as_list=True ,temperature=0.8, prefix=user_input, include_prefix=True, truncate='<|endoftext|>')[0]
     
-    return parse_text(text)
+    return text.replace('<|startoftext|>', '')
 
 def single_tweet():
     return gpt2.generate(sess, length=200, return_as_list=True, temperature=1, prefix='<|startoftext|>', include_prefix=False, truncate='<|endoftext|>')[0]
@@ -29,7 +29,7 @@ def single_tweet():
 if __name__ == "__main__":
     print("\n --------------------- \n")
 
-    # print(make_text(user_input))
-    print(single_tweet())
+    print(make_text('The RAF is'))
+    # print(single_tweet())
 
     print("\n --------------------- \n")
